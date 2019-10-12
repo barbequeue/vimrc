42e5a9f83d5da6be49aff1fe9eba4889af160738
Stas Bulanov <p99kab00@gmail.com>
2019-10-12 20:33:49 +0300

    Added install script

---
+++ install_vimrc.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+cd ~/.vim
+
+echo 'set runtimepath+=~/.vim
+
+source ~/.vim/plugins_list
+
+source ~/.vim/vimrcs/basic.vim
+source ~/.vim/vimrcs/filetypes.vim
+source ~/.vim/vimrcs/plugins_config.vim
+source ~/.vim/vimrcs/extended.vim
+
+try
+touch ~/.vim/my_configs.vim
+source ~/.vim/my_configs.vim
+catch
+endtry' > ~/.vimrc
