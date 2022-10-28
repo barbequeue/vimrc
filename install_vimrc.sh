cd70b8290b1d4367e54c8f595236f7e92fbb1a89
Stanislav Bulanov <p99kab00@gmail.com>
2022-10-28 22:28:52 +0300

    Move all vimrcs to nvim default config directory

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 set -e
 
+echo 'Creating ~/.config/nvim directory if not exists\n'
 mkdir -p ~/.config/nvim/
-echo 'set runtimepath+=~/.vim
 
-source ~/.vim/plugfile.vim
+echo 'Copying configuration file for plugins to be installed to ~/.config/nvim/\n'
+cp ./plugfile.vim ~/.config/nvim/
 
-source ~/.vim/vimrcs/basic.vim
-source ~/.vim/vimrcs/filetypes.vim
-source ~/.vim/vimrcs/plugins_config.vim
-source ~/.vim/vimrcs/extended.vim
+echo 'Copying Vim runtime configuration files ~/.config/nvim/\n'
+cp -r ./vimrcs/ ~/.config/nvim/
 
-try
-source ~/.vim/my_configs.vim
-catch
-endtry' >> ~/.config/nvim/init.vim
+echo 'Creating ~/.config/nvim/init.vim\n'
+echo "exec 'source ' .. stdpath('config') .. '/plugfile.vim'
+
+exec 'source ' .. stdpath('config') .. '/vimrcs/basic.vim'
+exec 'source ' .. stdpath('config') .. '/vimrcs/filetypes.vim'
+exec 'source ' .. stdpath('config') .. '/vimrcs/plugins_config.vim'
+exec 'source ' .. stdpath('config') .. '/vimrcs/extended.vim'" >> ~/.config/nvim/init.vim
