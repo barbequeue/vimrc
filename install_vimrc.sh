ca668372462d546ff049424ac82595c2f74695d3
Stanislav Bulanov <p99kab00@gmail.com>
2023-02-13 13:59:15 +0200

    Install script formatting

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 set -e
 
-echo 'Creating ~/.config/nvim directory if not exists\n'
+echo "Creating ~/.config/nvim directory if it don't exists"
 mkdir -p ~/.config/nvim/
 
-echo 'Copying configuration file for plugins to be installed to ~/.config/nvim/\n'
+echo "Copying plugins configuration to ~/.config/nvim/"
 cp ./plugfile.vim ~/.config/nvim/
 
-echo 'Copying Vim runtime configuration files ~/.config/nvim/\n'
+echo "Copying Vim runtime configuration to ~/.config/nvim/"
 cp -r ./vimrcs/ ~/.config/nvim/
 
-echo 'Creating ~/.config/nvim/init.vim\n'
+echo 'Creating ~/.config/nvim/init.vim'
 echo "exec 'source ' .. stdpath('config') .. '/plugfile.vim'
 
 exec 'source ' .. stdpath('config') .. '/vimrcs/basic.vim'
