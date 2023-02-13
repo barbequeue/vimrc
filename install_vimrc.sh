41a9047b8770b8613c63e38c640d8a8e0ff02147
Stanislav Bulanov <p99kab00@gmail.com>
2023-02-13 14:22:23 +0200

    Install plugins automatically with install script

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -17,3 +17,6 @@ exec 'source ' .. stdpath('config') .. '/vimrcs/basic.vim'
 exec 'source ' .. stdpath('config') .. '/vimrcs/filetypes.vim'
 exec 'source ' .. stdpath('config') .. '/vimrcs/plugins_config.vim'
 exec 'source ' .. stdpath('config') .. '/vimrcs/extended.vim'" >> ~/.config/nvim/init.vim
+
+echo 'Running NeoVim to install plugins listed in the configuration file'
+nvim -c ":echo 'Installing plugins...' | PlugInstall | echo 'Plugins installed. Run :RLD<Enter> to reload and start using NeoVim!'"
